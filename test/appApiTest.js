47ca4e0e70a2fd8a933f695aff0d3343e33ea809
vansin <vansin@aliyun.com>
2021-08-21 12:01:35 +0800

    feat: 新增appApi测试案列

--- test/appApiTest.js
+++ test/appApiTest.js
@@ -12,6 +12,17 @@ request.post('/app/adminAddAppId', {
 })
 
 request.post('/app/adminAddAppId', {
+    admin_name: 'admin',
+    app_id: 'debug_app_id',
+    app_name: 'debug',
+    app_desc: 'debug时使用的app_is',
+}).then( res=> {
+    console.log(res.data)
+}).catch(e => {
+    console.error(e)
+})
+
+request.post('/app/adminAddAppId', {
     admin_name: 'admin1',
     app_id: 'byte-monitor-docs',
     app_name: '字节监控对外文档',
