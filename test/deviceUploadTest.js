24912196b0b51f173945c9a0ad1bfc76050faa9f
vansin <vansin@aliyun.com>
2021-08-22 11:46:33 +0800

    feat: mock上报初步时间

--- test/deviceUploadTest.js
+++ test/deviceUploadTest.js
@@ -1,59 +1,36 @@
 const request = require('./request')
 
-// request.post('/device/getRecentXMinNums', {
-//     app_id: '114514114514abc',
-//     xMin: 10
-// }).then( res=> {
-//     console.log(res.data)
-// })
 
 
-// request.post('/device/getRecentXMinNums', {
-//     app_id: '114514114514abc',
-//     xMin: 60000000000000
-// }).then( res=> {
-//     console.log(res.data)
-// })
 
 
-// request.post('/device/getAllItemList', {
-//     app_id: '114514114514abc',
-//     skip: 0,
-//     limit: 3
-// }).then( res=> {
-//     console.log(res.data)
-// })
+const mock = (async () => {
+    let statTimeStamp = new Date(2021,7,10).getTime()
+    let nowTimeStamp = new Date().getTime()
+    console.log(statTimeStamp, nowTimeStamp)
+    while (statTimeStamp< nowTimeStamp) {
+        console.log(statTimeStamp)
+
+        let data = {
+        deviceType:"PC",
+        OS:"Linux",
+        OSVersion:"",
+        screenHeight:1200,
+        screenWidth:1920,
+        language:"en",
+        netWork:"4g",
+        orientation:"横屏",
+        browser:"Chrome",
+        browserInfo:"Chrome（版本: 92.0.4515.131&nbsp;&nbsp;内核: Blink）",
+        user_id:"PWNnPecyFp",
+        time: new Date(statTimeStamp),
+        app_id:"byte-monitor-docs"
+    }
+        await request.post('/device/upload',data)
+        statTimeStamp += 24*60*60 * 1000
+    }
 
 
-// request.post('/device/statXMinRecentPvBrowserRatio', {
-//     app_id: '114514114514abc',
-//     xMin: 60000000000000
-// }).then( res=> {
-//     console.log(res.data)
-// })
-
-
-// request.post('/device/statXMinRecentPvOsRatio', {
-//     app_id: '114514114514abc',
-//     xMin: 60000000000000
-// }).then( res=> {
-//     console.log(res.data)
-// })
-
-request.post('/device/statXDayPerDayPv', {
-    app_id: '114514114514abc',
-    xDay: 20
-}).then( res=> {
-    console.log(res.data)
-}).catch(e=>{
-    console.log(e)
 })
 
-// request.post('/device/statXHourPerHourPv', {
-//     app_id: '114514114514abc',
-//     xHour: 72
-// }).then( res=> {
-//     console.log(res.data)
-// }).catch(e=>{
-//     console.log(e)
-// })
+mock()
